d7f5701442e1a28bf86dcaeec3cdf5cfa631b4c6
Christian Diaz Portela <christiandp7@gmail.com>
2021-05-09 14:20:35 -0400

    useCallback class

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useReducer, useMemo, useRef } from 'react'
+import React, { useState, useEffect, useReducer, useMemo, useRef, useCallback } from 'react'
+import Search from './Search'
 
 const initialState = {
   favorites: []
@@ -34,10 +35,14 @@ const Characters = () => {
     dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
   }
 
-  const handleSearch = (event) => {
-    setSearch(searchInput.current.value)
-  }
-
+  // const handleSearch = (event) => {
+  //   setSearch(searchInput.current.value)
+  // }
+  
+  const handleSearch = useCallback(() => {
+      setSearch(searchInput.current.value)
+    }, [])
+  
   // const filteredUsers = characters.filter((user) => {
   //   return user.name.toLowerCase().includes(search.toLowerCase())
   // })
@@ -57,9 +62,11 @@ const Characters = () => {
         ))
       }
 
-      <div className="Search">
-        <input type="text" value={search} ref={searchInput} onChange={handleSearch} />
-      </div>
+      <Search 
+        search={search}
+        searchInput={searchInput}
+        handleSearch={handleSearch}
+      />
 
       {
         filteredUsers.map(character => (
